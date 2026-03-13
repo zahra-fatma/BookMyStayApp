6558232c8b7f750d41b18f4478bda2504d5a25e4
zahra-fatma <zahrafatma548@gmail.com>
2026-03-13 08:36:06 +0530

    Use Case 1: Application Entry & Welcome Message

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,16 @@
+
+public class UseCase1HotelBookingApp {
+
+
+    public static void main(String[] args) {
+
+
+        String appName = "Book My Stay - Hotel Booking Management System";
+        String version = "Version 1.0";
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+        System.out.println(appName);
+        System.out.println(version);
+        System.out.println("Application execution completed.");
+    }
+}
